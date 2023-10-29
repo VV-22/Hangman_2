4e068a3b51ef5986aea7ecff9a954a2f9ed48744
Vishnu Vardan <coolboyvishnuvardan@gmail.com>
2023-10-29 21:44:27 +0530

    code stuff
    vscode setup and starting to write controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+   [SerializeField]GameObject[] hangmanObjects;
+   [SerializeField]Transform wordTransform;
+   [SerializeField]Transform letterTransform;
+
+}
